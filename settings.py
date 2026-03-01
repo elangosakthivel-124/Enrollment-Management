cc72f601a3d84d12b60be200be8a3903580b03a3
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-01 23:04:01 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -72,3 +72,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
