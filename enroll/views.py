8cb719d4c8877e89f4272beeb6fc5541c303f2f8
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-03 23:12:27 +0530

    Create views.py

---
+++ enroll/views.py
@@ -0,0 +1,29 @@
+
+from django.shortcuts import render, redirect
+from .models import Student
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+
+        Student.objects.create(
+            name=name,
+            email=email,
+            phone=phone
+        )
+
+        return redirect("student_list")
+
+    return render(request, "add_student.html")
+
+
+def student_list(request):
+    students = Student.objects.all().order_by("-id")
+    return render(request, "student_list.html", {"students": students})
