8336678accad8494f2ceac9a347d62c5d9c47095
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-03 23:14:00 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from enroll.views import home
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path("admin/", admin.site.urls),
+    path("", include("enroll.urls")),
 ]
