dfc56173e12dbf810f43ad10a780b3b05134c966
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-06 23:49:06 +0530

    Add course-related URL patterns

--- enroll/url.py
+++ enroll/url.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path("", views.home, name="home"),
+
     path("add-student/", views.add_student, name="add_student"),
     path("students/", views.student_list, name="student_list"),
+
+    path("add-course/", views.add_course, name="add_course"),
+    path("courses/", views.course_list, name="course_list"),
+
 ]
