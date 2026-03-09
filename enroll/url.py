758ec7339986a1079575f57c396bbd39999b3d11
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-09 23:04:49 +0530

    Clean up URL patterns in enroll/url.py
    Removed unused URL patterns for students and courses.

--- enroll/url.py
+++ enroll/url.py
@@ -11,20 +11,6 @@ urlpatterns = [
     path("add-course/", views.add_course, name="add_course"),
     path("courses/", views.course_list, name="course_list"),
 
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-    path("", views.home, name="home"),
-
-    path("add-student/", views.add_student, name="add_student"),
-    path("students/", views.student_list, name="student_list"),
-
-    path("add-course/", views.add_course, name="add_course"),
-    path("courses/", views.course_list, name="course_list"),
-
     path("enroll/", views.enroll_student, name="enroll_student"),
     path("enrollments/", views.enrollment_list, name="enrollment_list"),
 
