b430a19a96c0b912423b8d7722f4346cedaa8e46
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-11 00:26:16 +0530

    Add edit and delete paths for students and courses

--- enroll/url.py
+++ enroll/url.py
@@ -7,9 +7,13 @@ urlpatterns = [
 
     path("add-student/", views.add_student, name="add_student"),
     path("students/", views.student_list, name="student_list"),
+    path("edit-student/<int:id>/", views.edit_student, name="edit_student"),
+    path("delete-student/<int:id>/", views.delete_student, name="delete_student"),
 
     path("add-course/", views.add_course, name="add_course"),
     path("courses/", views.course_list, name="course_list"),
+    path("edit-course/<int:id>/", views.edit_course, name="edit_course"),
+    path("delete-course/<int:id>/", views.delete_course, name="delete_course"),
 
     path("enroll/", views.enroll_student, name="enroll_student"),
     path("enrollments/", views.enrollment_list, name="enrollment_list"),
