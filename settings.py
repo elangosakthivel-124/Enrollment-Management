3c3b8773a68a993360da0aadbf32474d3505d858
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-17 23:38:21 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -78,3 +78,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
